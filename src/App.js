2e5bcdd4cfcfcb32c3913b9878a7507682d8ea86
Sandeep Dhamala <141273955+sandeep0600@users.noreply.github.com>
2024-04-17 23:56:23 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,11 +14,10 @@ function App() {
         <img src={logo} alt="Logo" className="girl" />
         <div className="content">
           <span className="heading">
-            Hi, I am Sandeep, a Full-stack developer...
+            Hi, I am Sandeep Dhamala...
           </span>
           <span className="paragraph">
-            I am a student web developer. I am currently improving my skills in web development in MERN stack.And I am truly an enthusiastic...
-          </span>
+I am a versatile professional with a robust skill set encompassing technical expertise, outstanding customer service, and proficiency in full-stack development and graphic design. In my role, I excel in diagnosing and resolving hardware and software issues, collaborating effectively with teams, and maintaining meticulous documentation. My experience in network operations includes overseeing infrastructure, addressing alerts promptly, identifying and remedying problems, and managing various networking tools such as Nokia AMS, Huawei NCE, SSRMS, Cacti, Zabbix, ACS, BNG, routing and switching, and Zero Touch Provisioning (ZTP). As a full-stack developer, I bring valuable experience in designing, developing, and deploying web applications, ensuring seamless user experiences and optimal functionality. Additionally, my expertise in graphic design adds a creative edge to my work, enhancing visual communication and branding strategies. I am dedicated to contributing effectively to network management, optimization, full-stack development, graphic design, and other areas, ensuring seamless operations and enhancing overall business performance.          </span>
           <div className="App">
           <a href="https://drive.google.com/file/d/1Y8QOGaai3QDOtA7wSgjAPz3o0GlpM5bK/view?usp=sharing" target="_blank" rel="noreferrer">
           <button className="resume">Download Resume</button>
